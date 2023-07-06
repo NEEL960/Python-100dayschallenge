d24a606b5c998872538f55e44c9c7fdb98298963
Neel Gabani <neelgabani24ng@gmail.com>
2023-07-07 00:11:11 +0530

    day 6

---
+++ DAY 6/day6.py
@@ -0,0 +1,34 @@
+#--------------------functions 
+
+
+def my_function():  #defining the function 
+    print("Hello, World!")
+    print("Bye")
+
+my_function()  # calling the function 
+
+
+#-----------------------Maze game-------------
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+    
+# while front_is_clear():
+#     move()
+# turn_left()
+
+# while not at_goal():
+#     if right_is_clear():
+#         turn_right()
+#         move()
+#     elif front_is_clear():
+#         move()
+#     else:
+#         turn_left()
+
+    
+
+        
+        
+        
